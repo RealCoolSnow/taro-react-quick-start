07e7d19d17fc0d0a3ee7a7472072c060a62309b0
CoolSnow <coolsnow2020@gmail.com>
2021-04-01 16:05:11 +0800

    update

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,23 +1,19 @@
 import { Component } from 'react'
-import { View, Text, Navigator, Button } from '@tarojs/components'
+import { View, Text, Navigator, Button, ITouchEvent } from '@tarojs/components'
 import Logo from '../../components/Logo'
 import './index.less'
+import { showAlert } from '../../utils/util'
 
 export default class Index extends Component {
-  componentWillMount() {}
-
-  componentDidMount() {}
-
-  componentWillUnmount() {}
-
-  componentDidShow() {}
-
-  componentDidHide() {}
-
+  private handleClickHello(event: ITouchEvent): void {
+    showAlert('hello', `${new Date().valueOf()}:${event.type}`)
+  }
   render() {
     return (
       <View className='flex flex-col items-center p-2'>
-        <Text className='mt-2'>Hello world!</Text>
+        <Text className='mt-2' onClick={this.handleClickHello.bind(this)}>
+          Hello world!
+        </Text>
         <View className='mt-2'>
           <Logo />
         </View>
