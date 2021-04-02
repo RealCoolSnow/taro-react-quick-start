2114afb3c1c653a07e1cd3274683b50911465f20
CoolSnow <coolsnow2020@gmail.com>
2021-04-02 11:29:09 +0800

    update

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,16 +1,36 @@
-import { View } from '@tarojs/components'
+import { Button, View } from '@tarojs/components'
 import React from 'react'
+import { connect } from 'react-redux'
+import { add, minus } from '../store/actions/counter'
 
-export interface IProps {
-  value: number
-  onIncrement: () => void
-  onDecrement: () => void
+interface IProps {
+  num: number
+  increment: () => void
+  decrement: () => void
 }
-export default class Counter extends React.PureComponent<IProps> {
+
+const mapStateToProps = (state: any) => {
+  return {
+    num: state.counter.num
+  }
+}
+const mapDispatchToProps = (dispatch: any) => ({
+  increment: () => dispatch(add),
+  decrement: () => dispatch(minus)
+})
+
+@(connect(mapStateToProps, mapDispatchToProps) as any)
+export default class Counter extends React.Component<IProps> {
   public componentDidMount() {
     console.log(this.props)
   }
   render() {
-    return <View>counter</View>
+    return (
+      <View>
+        <Button onClick={this.props.increment}>
+          click me {this.props.num}
+        </Button>
+      </View>
+    )
   }
 }
