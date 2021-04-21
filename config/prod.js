d1be406858a5a0a3edf1dfd5b7042c753161469a
CoolSnow <coolsnow2020@gmail.com>
2021-04-21 16:23:03 +0800

    remove console log in production mode

--- config/prod.js
+++ config/prod.js
@@ -1,10 +1,28 @@
+import TerserPlugin from 'terser-webpack-plugin'
+
 module.exports = {
   env: {
     NODE_ENV: '"production"'
   },
-  defineConstants: {
+  defineConstants: {},
+  mini: {
+    webpackChain(chain, webpack) {
+      chain.plugin('terser-webpack-plugin').use(TerserPlugin, [
+        {
+          terserOptions: {
+            ecma: undefined,
+            warnings: false,
+            parse: {},
+            compress: {
+              drop_console: true,
+              drop_debugger: false,
+              pure_funcs: ['console.log'] // 移除console
+            }
+          }
+        }
+      ])
+    }
   },
-  mini: {},
   h5: {
     /**
      * 如果h5端编译后体积过大，可以使用webpack-bundle-analyzer插件对打包体积进行分析。
